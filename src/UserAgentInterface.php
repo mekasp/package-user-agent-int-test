92f746a2c5fa169d486c8877df0d4e4dfdc47a88
mekasp <maximpapkov6@gmail.com>
2022-11-10 21:25:25 +0200

    v1.0.0

--- src/UserAgentInterface.php
+++ src/UserAgentInterface.php
@@ -2,6 +2,11 @@
 
 namespace Mekas\UserAgent\Int\Test;
 
+/**
+ * Interface UserAgentInterface
+ * @package Mekas\UserAgent\Int\Test v1.0.0
+ */
+
 interface UserAgentInterface
 {
     public function getBrowser(): ?string;
